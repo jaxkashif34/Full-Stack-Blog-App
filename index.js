658534757c2662d7d974b1cd286df1a72764cfa4
Kashif Ali <jaxksahif34@gmail.com>
2022-10-16 21:19:03 +0500

    deploying

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, 'build')));
 
 if (process.env.NODE_ENV === 'production') {
-  app.post('/*', (req, res) => {
+  app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build/index.html'));
   });
 }
